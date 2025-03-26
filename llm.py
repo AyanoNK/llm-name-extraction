e399df876c9dddbb12ccbae082f96fc4cb2d70ca
Ayano <s.correa.wilches@gmail.com>
2025-03-25 20:49:53 -0500

    add llm consumer handler

---
+++ llm.py
@@ -0,0 +1,54 @@
+"""LLM model inference utils"""
+
+import json
+
+from dotenv import dotenv_values
+import boto3
+
+
+# Set up the client
+
+config = dotenv_values(".env")
+
+
+AWS_ACCESS_KEY_ID = config.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config.get("AWS_SECRET_ACCESS_KEY")
+
+MODEL_NAME = "amazon.titan-text-lite-v1"
+
+
+def llm_inference(prompt: str):
+    """Call the Bedrock runtime API to get the response from the model
+
+    Args:
+        prompt (str): The prompt to send to the model
+
+    Raises:
+        ValueError: If the env vars are missing
+
+    Returns:
+        json_response (dict): The response from the model
+    """
+    if not AWS_ACCESS_KEY_ID or not AWS_SECRET_ACCESS_KEY:
+        raise ValueError(
+            "Missing env variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"
+        )
+
+    bedrock_runtime = boto3.client(
+        "bedrock-runtime",
+        region_name="us-east-1",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    )
+
+    kwargs = {
+        "modelId": MODEL_NAME,
+        "contentType": "application/json",
+        "accept": "*/*",
+        "body": json.dumps({"inputText": prompt}),
+    }
+
+    response = bedrock_runtime.invoke_model(**kwargs)
+    json_response: dict = json.loads(response.get("body").read())
+
+    return json_response
